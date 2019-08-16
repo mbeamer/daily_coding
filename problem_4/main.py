4dc144d81993cf900bea6bf58dbd599616d51d46
Matthew Beamer <matthew.beamer@gmail.com>
2019-08-16 14:42:50 -0400

    Cleanup and use asserts rather than echoes.

--- problem_4/main.py
+++ problem_4/main.py
@@ -7,15 +7,12 @@
 
     You can modify the input array in-place.
 '''
-import sys
-
 SAMPLE_1 = [3, 4, -1, 1]
 ANSWER_1 = 2
 
 SAMPLE_2 = [1, 2, 0]
 ANSWER_2 = 3
 
-MAX_INT = 99
 def main(data):
     '''
     Find the lowest missing integer.
@@ -23,21 +20,18 @@ def main(data):
     data is a list of integers
     '''
     data.sort(reverse=True)
-    print("Sorted: {}".format(data))
     for index, datum in enumerate(data):
-        print("datum: {}".format(datum))
-
         if index == 0:
             continue
         if datum < 0:
             continue
 
+        # Since we are reverse sorted, check that the last number is only one larger than us.
         if datum < data[index-1]-1:
             return datum+1
 
     return data[0]+1
 
 if __name__ == "__main__":
-    SMALLEST = main(SAMPLE_1)
-    print("Smallest missing integer: {}".format(SMALLEST))
-    sys.exit(0)
+    assert main(SAMPLE_1) == ANSWER_1
+    assert main(SAMPLE_2) == ANSWER_2
